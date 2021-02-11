b006ca21c01e8e398a929358c3af70ffe8ef1cfe
BrandonMFong <fong.m.brandon97@gmail.com>
2021-02-10 17:13:25 -0800

    Cleaning up

--- 1027/main.go
+++ 1027/main.go
@@ -15,13 +15,11 @@ import (
 	"log"
 	"os"
 	"strings"
-	"sync"
 )
 
 func main() {
 	var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	var keyLength = 5
-	// var popularLettersInText = "EHSTMAQOPBNDHIX"
 	var popularLettersInText = "EHSTDHIX"
 	var size int
 	var index int
@@ -116,7 +114,6 @@ func main() {
 		// If not, then include as possible keys
 		if !dontInsert {
 			possibleKeys[index] = tempString
-			// fmt.Println(tempString)
 			index++
 		}
 	})
@@ -192,6 +189,8 @@ func readLines(path string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
+/* Credits to:  https://yourbasic.org/golang/generate-permutation-slice-string/ */
+
 // Perm calls f with each permutation of a.
 func Perm(a []rune, f func([]rune)) {
 	perm(a, f, 0)
@@ -209,90 +208,4 @@ func perm(a []rune, f func([]rune), i int) {
 		perm(a, f, i+1)
 		a[i], a[j] = a[j], a[i]
 	}
-}
-
-func generate(alphabet string) <-chan string {
-	c := make(chan string, len(alphabet))
-
-	go func() {
-		defer close(c)
-
-		if len(alphabet) == 0 {
-			return
-		}
-
-		// Use a sync.WaitGroup to spawn permutation
-		// goroutines and allow us to wait for them all
-		// to finish.
-		var wg sync.WaitGroup
-		wg.Add(len(alphabet))
-
-		for i := 1; i <= len(alphabet); i++ {
-			go func(i int) {
-				// Perform permutation on a subset of
-				// the alphabet.
-				Word(alphabet[:i]).Permute(c)
-
-				// Signal Waitgroup that we are done.
-				wg.Done()
-			}(i)
-		}
-
-		// Wait for all routines to finish.
-		wg.Wait()
-	}()
-
-	return c
-}
-
-type Word []rune
-
-// Permute generates all possible combinations of
-// the current word. This assumes the runes are sorted.
-func (w Word) Permute(out chan<- string) {
-	if len(w) <= 1 {
-		out <- string(w)
-		return
-	}
-
-	// Write first result manually.
-	out <- string(w)
-
-	// Find and print all remaining permutations.
-	for w.next() {
-		out <- string(w)
-	}
-}
-
-// next performs a single permutation by shuffling characters around.
-// Returns false if there are no more new permutations.
-func (w Word) next() bool {
-	var left, right int
-
-	left = len(w) - 2
-	for w[left] >= w[left+1] && left >= 1 {
-		left--
-	}
-
-	if left == 0 && w[left] >= w[left+1] {
-		return false
-	}
-
-	right = len(w) - 1
-	for w[left] >= w[right] {
-		right--
-	}
-
-	w[left], w[right] = w[right], w[left]
-
-	left++
-	right = len(w) - 1
-
-	for left < right {
-		w[left], w[right] = w[right], w[left]
-		left++
-		right--
-	}
-
-	return true
 }
